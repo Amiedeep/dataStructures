aa51d7f7e09c4bb456387e1be20c84f9797abde4
Amandeep Singh <amandees@thoughtworks.com>
2018-11-03 12:41:29 +0530

    merge sort for lists

--- src/LList.java
+++ src/LList.java
@@ -1,6 +1,10 @@
 
 import java.util.Scanner;
-public class LList 
+
+import static java.util.Objects.isNull;
+import static java.util.Objects.nonNull;
+
+public class LList
 {
     LListNode root;
     void insert(int value)//to insert new node with value at the last of linked list
@@ -86,39 +90,103 @@ public class LList
     }
     public static void main(String[] args) 
     {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("enter 1 to insert,2 to delete,3 o traverse,4 to reverse,5 to  search,6 to exit");
-        int n=sc.nextInt();
-        LList obj=new LList();
-        while(n!=6)
-        {
-            switch(n)
-            {
-                case 1:
-                    System.out.println("enter value to insert");
-                    obj.insert(sc.nextInt());
-                    break;
-                case 2:
-                    System.out.println("enter value to delete");
-                    obj.delete(sc.nextInt());
-                    break;
-                case 3:
-                    obj.traverse();
-                    break;
-                case 4:
-                    obj.reverse();
-                    break;
-                case 5:
-                    System.out.println("enter value to search");
-                    obj.search(sc.nextInt());
-                    break;
-               
-                default:
-                    System.out.println("please enter valid input");
-                    break;
-            }
-            System.out.println("enter 1 to insert,2 to delete,3 o traverse,4 to reverse,5 to search,6 to exit");
-            n=sc.nextInt();
+//        Scanner sc=new Scanner(System.in);
+//        System.out.println("enter 1 to insert,2 to delete,3 o traverse,4 to reverse,5 to  search,6 to exit");
+        LList linkedList = new LList();
+        linkedList.insert(40);
+        linkedList.insert(20);
+        linkedList.insert(60);
+        linkedList.insert(10);
+//        linkedList.insert(50);
+        linkedList.insert(30);
+        linkedList.root = linkedList.mergeSort(linkedList.root);
+        linkedList.printList();
+
+//        int n=sc.nextInt();
+//        while(n!=6)
+//        {
+//            switch(n)
+//            {
+//                case 1:
+//                    System.out.println("enter value to insert");
+//                    obj.insert(sc.nextInt());
+//                    break;
+//                case 2:
+//                    System.out.println("enter value to delete");
+//                    obj.delete(sc.nextInt());
+//                    break;
+//                case 3:
+//                    obj.traverse();
+//                    break;
+//                case 4:
+//                    obj.reverse();
+//                    break;
+//                case 5:
+//                    System.out.println("enter value to search");
+//                    obj.search(sc.nextInt());
+//                    break;
+//
+//                default:
+//                    System.out.println("please enter valid input");
+//                    break;
+//            }
+//            System.out.println("enter 1 to insert,2 to delete,3 o traverse,4 to reverse,5 to search,6 to exit");
+//            n=sc.nextInt();
+//        }
+    }
+
+    private void printList() {
+        LListNode node = root;
+        while (nonNull(node)) {
+            System.out.println(node.data);
+            node = node.next;
         }
+    }
+
+    private LListNode mergeSort(LListNode node) {
+        if(length(node) < 2)
+            return node;
+        LListNode middleNode = findMiddle(node);
+        LListNode secondHalf = middleNode.next;
+        middleNode.next = null;
+        LListNode leftList = mergeSort(node);
+        LListNode rightList = mergeSort(secondHalf);
+
+        return sortLists(leftList, rightList);
+    }
+
+    private LListNode sortLists(LListNode firstNode, LListNode secondNode) {
+        if (isNull(firstNode))
+            return secondNode;
+        else if(isNull(secondNode))
+            return firstNode;
+        if(firstNode.data > secondNode.data) {
+            secondNode.next = sortLists(firstNode, secondNode.next);
+            return secondNode;
+        }
+        else {
+            firstNode.next = sortLists(firstNode.next, secondNode);
+            return firstNode;
+        }
+
+    }
+
+    private int length(LListNode node) {
+        int count = 0;
+        while (node != null) {
+            count++;
+            node = node.next;
+        }
+        return count;
+    }
+
+    private LListNode findMiddle(LListNode node) {
+        LListNode slowPointer = node;
+        LListNode fastPointer = node;
+        while(fastPointer.next != null && fastPointer.next.next != null) {
+            slowPointer = slowPointer.next;
+            fastPointer = fastPointer.next.next;
+        }
+        return slowPointer;
     }
 }
